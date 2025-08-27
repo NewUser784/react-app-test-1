62550059183031fc16e4f637016b6d4899d32873
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-08-27 13:49:16 -0600

    Create module to practice

---
+++ src/FavoriteFood.jsx
@@ -0,0 +1,10 @@
+function FavoriteFood() {
+    return (
+        <>
+            <h2>Favorite Food:</h2>
+            <p>Someone's favorite food is Pizza</p>
+        </>
+    )
+}
+
+export default FavoriteFood;
